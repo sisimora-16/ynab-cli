f44f37ba4656e158d75150ab49c19574ea16e6af
sisimora-16 <sisimora.16@gmail.com>
2024-09-20 20:11:15 -0400

    Added configuration logic

--- src/ynab/config/settings.py
+++ src/ynab/config/settings.py
@@ -1,22 +1,22 @@
 from ynab.config.config_manager import save_config, load_config, delete_config
 
-TOKEN_FILE = "token.json"
-BUDGET_ID_FILE = "budget_id.json"
+TOKEN_SETTINGS = "token.json"
+BUDGET_SETTINGS = "budget_id.json"
 
 def save_token(token):
-    save_config({"token": token}, TOKEN_FILE)
+    save_config({"token": token}, TOKEN_SETTINGS)
 
 def save_budget_id(budget_id):
-    save_config({"budget_id": budget_id}, BUDGET_ID_FILE)
+    save_config({"budget_id": budget_id}, BUDGET_SETTINGS)
 
 def load_token():
-    return load_config("token", TOKEN_FILE) 
+    return load_config("token", TOKEN_SETTINGS) 
 
 def load_budget_id():
-    return load_config("budget_id", BUDGET_ID_FILE)
+    return load_config("budget_id", BUDGET_SETTINGS)
 
 def delete_token():
-    delete_config(TOKEN_FILE)
+    delete_config(TOKEN_SETTINGS)
 
 def delete_budget_id():
-    delete_config(BUDGET_ID_FILE)
+    delete_config(BUDGET_SETTINGS)
